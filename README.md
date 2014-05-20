5f48822033a74d7e6237c503700073fbea894915
Colin Sullivan <colin@colin-sullivan.net>
2014-05-19 20:42:06 -0700

    Works with a single import line in a less file.

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 less-fontawesome-4
 ==================
 
+Usage:
+
+Add this single line to any `.less` file:
+
+```less
+@import "/packages/less-fontawesome-4/font-awesome.import.less";
+```
+
 For my [stylesheet boiler](https://github.com/DerMambo/stylesheets.git) and created for my blogpost:
 
 [Meteorjs and Twitter Bootstrap - The Right Way](www.manuel-schoebel.com/blog/meteorjs-and-twitter-bootstrap---the-right-way)
--- package.js
+++ package.js
@@ -1,3 +1,17 @@
 Package.describe({
   summary: 'Less version of Fontawesome 4 for Meteor.js'
-})
+});
+
+Package.on_use(function (api) {
+  api.use("less", "client");
+
+  api.add_files([
+    "./lib/fonts/FontAwesome.otf",
+    "./lib/fonts/fontawesome-webfont.woff",
+    "./lib/fonts/fontawesome-webfont.ttf",
+    "./lib/fonts/fontawesome-webfont.svg",
+    "./lib/fonts/fontawesome-webfont.eot"
+  ], "client");
+});
+
+
